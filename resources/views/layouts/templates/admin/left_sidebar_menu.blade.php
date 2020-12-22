8d0cc2e2cec0bd2d256ac6f8544ff3f8b6f7b995
Abdul Majid <abdulmajidcse@gmail.com>
2020-12-22 23:00:31 +0600

    change icon

--- resources/views/layouts/templates/admin/left_sidebar_menu.blade.php
+++ resources/views/layouts/templates/admin/left_sidebar_menu.blade.php
@@ -40,7 +40,7 @@
           {{-- Media --}}
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-layer-group"></i>
+              <i class="nav-icon fas fa-photo-video"></i>
               <p>
                 Media
                 <i class="fas fa-angle-left right"></i>
