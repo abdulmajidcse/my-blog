4777a636ff94d1f411c7ca80efc6b27d500f665e
Abdul Majid <abdulmajidcse@gmail.com>
2020-12-23 14:57:41 +0600

    customize header menu

--- resources/views/layouts/templates/frontend/header_menu.blade.php
+++ resources/views/layouts/templates/frontend/header_menu.blade.php
@@ -4,10 +4,10 @@
 @endphp  
   
   <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand-md navbar-dark navbar-primary">
+  <nav class="main-header navbar navbar-expand-lg navbar-dark navbar-primary">
     <div class="container">
 
-      <a href="{{ route('frontend.home') }}" class="navbar-brand d-md-none">
+      <a href="{{ route('frontend.home') }}" class="navbar-brand">
         <img src="{{ $setting ? asset('assets/uploads/'.$setting->app_logo) : asset('assets/static_uploads/abdulmajid.jpg') }}" alt="{{ $setting ? $setting->app_name : config('app.name') }}" class="brand-image img-circle elevation-3" style="opacity: .8">
       </a>
 
@@ -22,42 +22,20 @@
             <a href="{{ route('frontend.home') }}" class="nav-link">Home</a>
           </li>
 
-          <li class="nav-item dropdown">
-            <a id="categoriesDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link">Categories</a>
-            <ul aria-labelledby="categoriesDropdown" class="dropdown-menu dropdown-menu-xl border-0 shadow">
-              <li class="m-2 text-center">
-                @php
-                    $blogCategories = \App\Models\BlogCategory::orderBy('id', 'desc')->get();
-                @endphp
-                @if (count($blogCategories) > 0)
-                  @foreach ($blogCategories as $blogCategory)
-                    <a href="{{ route('frontend.blog.category', $blogCategory->slug) }}" class="btn btn-sm btn-flat btn-outline-primary mb-1">{{ $blogCategory->name }}</a>
-                  @endforeach
-                @else
-                  <span class="btn btn-flat btn-outline-danger">No Category Available.</span>
-                @endif
-                 
-              </li>
-            </ul>
-          </li>
-
-          <li class="nav-item dropdown">
-            <a id="contactDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link">Contact</a>
-            <ul aria-labelledby="contactDropdown" class="dropdown-menu dropdown-menu-lg border-0 shadow">
-              <li class="m-2 text-center">
-                <a href="{{ $setting ? $setting->youtube_link : '#' }}" class="btn btn-sm btn-flat btn-outline-danger mb-1" title="YouTube">YouTube</a>
-                <a href="{{ $setting ? $setting->facebook_link : '#' }}" class="btn btn-sm btn-flat btn-outline-primary mb-1" title="Facebook">Facebook</a>
-                <a href="{{ $setting ? $setting->linkedin_link : '#' }}" class="btn btn-sm btn-flat btn-outline-info mb-1" title="LinkedIn">LinkedIn</a>
-                <a href="{{ $setting ? $setting->github_link : '#' }}" class="btn btn-sm btn-flat btn-outline-dark mb-1" title="Github">Github</a>
-                <a href="{{ $setting ? $setting->twitter_link : '#' }}" class="btn btn-sm btn-flat btn-outline-primary mb-1" title="Twitter">Twitter</a>
-             </li>
-            </ul>
-          </li>
+          {{-- Blog Category --}}
+          @php
+              $blogCategories = \App\Models\BlogCategory::orderBy('id', 'desc')->limit(3)->get();
+          @endphp
+          @foreach ($blogCategories as $blogCategory)
+            <li class="nav-item">
+              <a href="{{ route('frontend.blog.category', $blogCategory->slug) }}" class="nav-link">{{ $blogCategory->name }}</a>
+            </li>
+          @endforeach
 
         </ul>
 
         <!-- SEARCH FORM -->
-        <form class="form-inline ml-0 ml-md-3" id="search-form">
+        <form class="form-inline ml-0 ml-lg-3" id="search-form">
           <div class="input-group input-group-sm">
             @if(isset($searchValue))
               <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search" id="search-value" value="{{ $searchValue }}">
