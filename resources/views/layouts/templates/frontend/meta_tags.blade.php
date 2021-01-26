d17c86e895b75ae7982b6450e47149b0bbd4d226
Abdul Majid <abdulmajidcse@gmail.com>
2021-01-26 10:05:38 +0600

    update meta tags.

--- resources/views/layouts/templates/frontend/meta_tags.blade.php
+++ resources/views/layouts/templates/frontend/meta_tags.blade.php
@@ -1,58 +1,49 @@
 {{-- <meta name='google-site-verification' content="" /> --}}
 <!-- [ Meta Tag SEO ] -->
-<meta name="robots" content="ALL, FOLLOW,INDEX" />
+<meta name="robots" content="ALL, FOLLOW, INDEX" />
 <meta name='language' content='English, Bangla'/>
 <meta name='url' content="{{ Request::url() }}" />
 <meta name='identifier-URL' content="{{ Request::url() }}" />
-<meta name='type' content='article' />
+<meta name='type' content='article, blog, portfolio' />
 <meta name="author" content="Abdul Majid" />
 <meta property="og:url" content="{{ Request::url() }}" />
-<meta property="og:type" content="article" />
+<meta property="og:type" content="article, blog, portfolio" />
 
 {{-- Default meta keyword and meta description --}}
 @php
     // Setting
     $setting = \App\Models\Setting::first();
 
-    $defaultTitle       = $setting ? $setting->app_title : 'A Bengali Blog About ICT';
-    $defaultKeyword     = $setting ? $setting->meta_keyword : 'HTML, CSS, JavaScript, JQuery, Reactjs, Vuejs, PHP, MySQL, WordPress, Laravel';
-    $defaultDescription = $setting ? $setting->meta_description : 'This is a bengali blog about ICT. You can get more information about ICT. I mean Web Design, Development, Computer Science, Software Development etc.';
-    $defaultImage       = $setting ? 'assets/uploads/'.$setting->meta_image : 'assets/static_uploads/abdulmajid.jpg';
+    $defaultTitle       = $setting && $setting->app_title ? $setting->app_title : 'A Blog About ICT';
+    $defaultKeyword     = $setting && $setting->seo_keyword ? $setting->seo_keyword : 'HTML, CSS, JavaScript, JQuery, Reactjs, Vuejs, PHP, MySQL, WordPress, Laravel';
+    $defaultDescription = $setting && $setting->seo_description ? $setting->seo_description : 'This is a blog about ICT. You can get more information about ICT. I mean Web Design, Development, Computer Science, Software Development etc.';
+    $defaultImage       = $setting && $setting->seo_image ? 'assets/uploads/'.$setting->seo_image : 'assets/static_uploads/abdulmajid.jpg';
 @endphp
 
 @if (isset($blogPost))
     
     {{-- Single post page --}}
     <meta name="title" content="{{ $blogPost->name }}" />
-    <meta name="description" content="{{ $blogPost->meta_description }}" />
-    <meta name="keywords" content="{{ $blogPost->meta_keyword }}" />
-    @if ($blogPost->image)
-        <meta name="image" content="{{ asset('assets/uploads/'.$blogPost->image) }}" />
-    @else
-        <meta name="image" content="{{ asset($defaultImage) }}" />
-    @endif
+    <meta name="description" content="{{ $blogPost->seo_description ? $blogPost->seo_description : $defaultDescription }}" />
+    <meta name="keywords" content="{{ $blogPost->seo_keyword ? $blogPost->seo_keyword : $defaultKeyword }}" />
+    <meta name="image" content="{{ $blogPost->image ? asset('assets/uploads/'.$blogPost->image) : asset($defaultImage) }}" />
 
     <meta property="og:title" content="{{ $blogPost->name }}" />
-    <meta property="og:description" content="{{ $blogPost->meta_description }}">
-    <meta property="og:keywords" content="{{ $blogPost->meta_keyword }}" />
-
-    @if ($blogPost->image)
-        <meta property="og:image" content="{{ asset('assets/uploads/'.$blogPost->image) }}" />
-    @else
-        <meta property="og:image" content="{{ asset($defaultImage) }}" />
-    @endif
+    <meta property="og:description" content="{{ $blogPost->seo_description ? $blogPost->seo_description : $defaultDescription }}">
+    <meta property="og:keywords" content="{{ $blogPost->seo_keyword ? $blogPost->seo_keyword : $defaultKeyword }}" />
+    <meta property="og:image" content="{{ $blogPost->image ? asset('assets/uploads/'.$blogPost->image) : asset($defaultImage) }}" />
 
 @elseif (isset($blogPosts) && isset($blogCategory))
     
     {{-- Posts by Category page --}}    
     <meta name="title" content="{{ $blogCategory->name }}" />
-    <meta name="description" content="{{ $blogCategory->meta_description }}" />
-    <meta name="keywords" content="{{ $blogCategory->meta_keyword }}" />
+    <meta name="description" content="{{ $blogCategory->seo_description ? $blogCategory->seo_description : $defaultDescription }}" />
+    <meta name="keywords" content="{{ $blogCategory->seo_keyword ? $blogCategory->seo_keyword : $defaultKeyword }}" />
     <meta name="image" content="{{ asset($defaultImage) }}" />
 
     <meta property="og:title" content="{{ $blogCategory->name }}" />
-    <meta property="og:description" content="{{ $blogCategory->meta_description }}">
-    <meta property="og:keywords" content="{{ $blogCategory->meta_keyword }}" />
+    <meta property="og:description" content="{{ $blogCategory->seo_description ? $blogCategory->seo_description : $defaultDescription }}">
+    <meta property="og:keywords" content="{{ $blogCategory->seo_keyword ? $blogCategory->seo_keyword : $defaultKeyword }}" />
     <meta property="og:image" content="{{ asset($defaultImage) }}" />
 
 @else
