880793abf6c2fce991043efafe80dbb359f9ed36
Abdul Majid <abdulmajidcse@gmail.com>
2022-01-10 14:39:43 +0600

    update auth middleware

--- laravel/app/Http/Middleware/Authenticate.php
+++ laravel/app/Http/Middleware/Authenticate.php
@@ -15,8 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return abort(404);
-            // return route('login');
+            return route('login');
         }
     }
 }
